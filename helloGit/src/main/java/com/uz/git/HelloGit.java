b598932b857ce639b73a5b71549abdc67d4cf23c
uzhou <2290280646@qq.com>
2020-08-26 23:32:16 +0800

    添加文件

---
+++ helloGit/src/main/java/com/uz/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.uz.git;
+
+/**
+ * @author MSI-NB zhouzhe
+ * @date 2020-08-26 23:22
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
